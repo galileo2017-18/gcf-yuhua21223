5ba3141adacccc09280e975939c1ebf682318a31
dantan <tand@galileoweb.org>
2015-01-06 13:30:02 -0800

    Create GCF.java

---
+++ GCF.java
@@ -0,0 +1,48 @@
+
+public class GCF {
+
+	public static void main(String[] args) {
+		//test your methods here
+		//make sure gcfIterative and gcfRecursive return the same value!
+		
+	}
+	
+	
+	//Returns GCF of a and b (iterative)
+	//Precondition: a > 0, b > 0
+	public static int gcfIterative (int a, int b) {
+	    while (a != b) {
+	        if (a > b)
+	            a -= b;
+	        else
+	            b -= a;
+	    }
+	    return a;
+	}
+
+	
+	//Returns GCF of a and b (recursive)
+	//Precondition: a > 0, b > 0
+	public static int gcfRecursive (int a, int b) {
+
+	    //you will write this in today’s lab
+		
+
+
+
+
+	}
+	
+	
+	//Challenge: rewrite gcfIterative and gcfRecursive with the 
+	// modulo operator % to make the methods more efficient
+	
+	public static int gcfIterMod (int a, int b) {
+		
+	}
+	
+	public static int gcfRecurMod (int a, int b) {
+		
+	}
+
+}
